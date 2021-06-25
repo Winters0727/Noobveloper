e203a01a8c86c5db9de17139c6025f780825b986
winters0727 <winters0727@gmail.com>
2021-06-25 14:12:33 +0900

    feature / add profile image by multer

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
 
 var apiRouter = require('./routes/api');
 
@@ -16,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/media', express.static(path.join(__dirname, 'media')));
 
 app.use(`/api/${process.env.VERSION}`, apiRouter);
 
