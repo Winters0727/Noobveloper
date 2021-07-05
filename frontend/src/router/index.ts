0b10788a79284ca411bc0afe5c751fb9c88165fa
winters0727 <winters0727@gmail.com>
2021-07-05 15:39:51 +0900

    create frontend folder

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+
+// component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
